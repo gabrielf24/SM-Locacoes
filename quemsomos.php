33f96391238199d6cdfc86725ff17b270fe3a65b
dantas <lucas01592@gmail.com>
2024-10-09 11:47:39 -0300

    quem somos

--- quemsomos.php
+++ quemsomos.php
@@ -1,1 +1,44 @@
-teste
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/estilo.css">
+    <script src="js/bootstrap.min.js"></script>
+    <title>Quem Somos</title>
+</head>
+<body>
+
+<nav class="barranav">
+<header class="p-3 bg-dark text-white">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+          <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+        </a>
+
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="index.php" class="nav-link px-2 text-secondary">Página Inicial</a></li>
+          <li><a href="quemsomos.php" class="nav-link px-2 text-white">Quem Somos</a></li>
+          <li><a href="produtos.php" class="nav-link px-2 text-white">Produtos</a></li>
+          <li><a href="contato.php" class="nav-link px-2 text-white">Contato</a></li>
+        </ul>
+
+        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+          <input type="search" class="form-control form-control-dark" placeholder="Pesquisa..." aria-label="Search">
+        </form>
+        
+        <div class="text-end">
+          <button type="button" class="btn btn-outline-light me-2">Login</button>
+          <button type="button" class="btn btn-warning">Cadastro</button>
+        </div>
+      </div>
+    </div>
+  </header>
+</nav>
+
+    
+</body>
+</html>
