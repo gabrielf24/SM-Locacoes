cca6bcdeead0dc16bdaa629f471e7d30ffc0e837
dantas <lucas01592@gmail.com>
2024-10-18 11:28:42 -0300

    produtos

---
+++ produtos.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>SM Locações</title>
+</head>
+<body>
+    
+<!-- inclusão do cabeçalho -->
+<?php include ("cabecalho.php") ?>
+
+<div class = "main">
+<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 hidden-xs">
+  <div class="col-left sidebar">
+    <div class="block block-layered-nav">
+      <div class="bx-title new-title">
+        <h2>CATEGORIAS</h2>
+      </div>
+      <div class="block-content">
+        <ul class="cd-accordion-menu animated">
+         
+                    
+            <li><img src="https://rentalfesta.com.br/icones/icone_copos-e-tacas.png" class="icone"><a href="https://rentalfesta.com.br/catalogo/copos-e-tacas">Copos e Taças</a></li>
+            <li><img src="https://rentalfesta.com.br/icones/icone_cristais.png" class="icone"><a href="https://rentalfesta.com.br/catalogo/cristais">Cristais</a></li> 
+            <li><img src="https://rentalfesta.com.br/icones/icone_inox.png" class="icone"><a href="https://rentalfesta.com.br/catalogo/inox">Inox</a></li>
+            <li><img src="https://rentalfesta.com.br/icones/icone_mesas-e-cadeiras.png" class="icone"><a href="https://rentalfesta.com.br/catalogo/mesas-e-cadeiras">Mesas / Cadeiras / Pranchão</a></li>
+            <li><img src="https://rentalfesta.com.br/icones/icone_prata.png" class="icone"><a href="https://rentalfesta.com.br/catalogo/prata">Prata</a></li>
+            <li><img src="https://rentalfesta.com.br/icones/icone_pratos.png" class="icone"><a href="https://rentalfesta.com.br/catalogo/pratos">Pratos</a></li><li><img src="https://rentalfesta.com.br/icones/icone_sous-plat.png" class="icone"><a href="https://rentalfesta.com.br/catalogo/sous-plat">Sousplat</a></li>
+            <li><img src="https://rentalfesta.com.br/icones/icone_suqueiras.png" class="icone"><a href="https://rentalfesta.com.br/catalogo/suqueiras">Suqueiras</a></li>
+            <li><img src="https://rentalfesta.com.br/icones/icone_talheres-e-acessorios.png" class="icone"><a href="https://rentalfesta.com.br/catalogo/talheres-e-acessorios">Talheres</a></li>
+            <li><img src="https://rentalfesta.com.br/icones/icone_utensilios.png" class="icone"><a href="https://rentalfesta.com.br/catalogo/utensilios">Utensílios para Cozinha</a></li>
+            <li><img src="https://rentalfesta.com.br/icones/icone_vidros-e-saladeiras.png" class="icone"><a href="https://rentalfesta.com.br/catalogo/vidros-e-saladeiras">Vidros</a></li>
+            <li><img src="https://rentalfesta.com.br/icones/icone_xicaras-e-canecas.png" class="icone"><a href="https://rentalfesta.com.br/catalogo/xicaras-e-canecas">Xícaras e Canecas</a></li>            
+            
+          
+        </ul>
+      </div>
+    </div>
+    <!--<div class="banner-left"> <img src="image/banner-left2.png" alt=""> </div>--> 
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- inclusão do rodapé -->
+<?php include ("rodape.php") ?>
+
+    
+    
+</body>
+
+<script src="js/bootstrap.min.js"></script>
+
+</html>
