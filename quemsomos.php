57f00fbb77926577c60fe20b1be44c0465b3e444
mateusT <mateuscas78@gmail.com>
2024-10-18 11:35:24 -0300

    Inicio da criação página quem somos

--- quemsomos.php
+++ quemsomos.php
@@ -11,9 +11,20 @@
 <!-- inclusão do cabeçalho -->
 <?php include ("cabecalho.php") ?>
 
-<main class="">
-    
-</main>
+
+    <div class="container-fluid text-center bg-secondary">
+        <h1 class="bg-primary">Visual teste</h1>
+    <h1>Quem Somos</h1>    
+<br>
+    <h2>A SM Locações é uma empresa especializada na locação de materiais para festas, incluindo itens como pratos, talheres, copos, taças e baldes.
+    <br>    
+    Atuando há quatro anos no mercado, a empresa é gerida por um casal e busca constantemente melhorar seus negócios e a qualidade do atendimento ao cliente</h2>
+    <br>
+    <h3>
+        Veja um pouco do nosso trabalho:
+    </h3>
+    </div>
+
 
 <!-- inclusão do rodapé -->
 <?php include ("rodape.php") ?>
