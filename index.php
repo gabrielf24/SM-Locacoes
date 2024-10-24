21895464da6b1ed80ba89de12127cfbdef09f975
mateusT <mateuscas78@gmail.com>
2024-10-24 09:10:36 -0300

    Adiçao do carousel na pagina index

--- index.php
+++ index.php
@@ -15,6 +15,29 @@
 <!-- inclusão do cabeçalho -->
 <?php include ("cabecalho.php") ?>
 
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/fotos da page quem somos/mesa decorada 3.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/fotos da page quem somos/mesa-decorada1.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/fotos da page quem somos/mesa-decorada2.jpeg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+
 
 <!-- inclusão do rodapé -->
 <?php include ("rodape.php") ?>
